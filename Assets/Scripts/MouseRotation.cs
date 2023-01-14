6dd859cceeb204a9206e1f8a176428a1752e1b72
Aleksey Martinovich <martinovichmain@gmail.com>
2023-01-14 15:48:56 +0300

    add custom cameras

---
+++ Assets/Scripts/MouseRotation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+public class MouseRotation: MonoBehaviour
+{
+    public float minXAngle;
+    public float maxXAngle;
+
+    public bool useYClamp = true;
+    public float minYAngle;
+    public float maxYAngle;
+
+    public float sensitivity;
+
+    public Vector3 initialRotation;
+
+    private float _rotationX;
+    private float _rotationY;
+
+    private void Awake()
+    {
+        transform.localEulerAngles = initialRotation;
+        _rotationX = initialRotation.x;
+        _rotationY = initialRotation.y;
+    }
+
+    private void LateUpdate()
+    {
+        _rotationX -= Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        _rotationY += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+
+        _rotationX = Mathf.Clamp(_rotationX, minXAngle, maxXAngle);
+        if(useYClamp)
+            _rotationY = Mathf.Clamp(_rotationY, minYAngle, maxYAngle);
+
+        transform.localEulerAngles = new Vector3(_rotationX, _rotationY, 0);
+
+
+    }
+}
